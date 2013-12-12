775673df62d9d6fb91d145ae667ca8547fc67a34
Atila Neves <atila.neves@gmail.com>
2013-12-12 21:45:57 +0100

    Fixed Windows console output

--- lib/console.hpp
+++ lib/console.hpp
@@ -1,7 +1,7 @@
 #ifndef CONSOLE_HPP_
 #define CONSOLE_HPP_
 
-#    ifndef WIN32
+#    ifndef _WIN32
 #        define ESC_GREEN "\033[32;1m"
 #        define ESC_RED "\033[31;1m"
 #        define ESC_CANCEL "\033[0;;m"
