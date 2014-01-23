02db6ccb70954c023efc180308117b8a0b8ff81d
Atila Neves <aalvesne@cisco.com>
2014-01-23 15:23:21 +0100

    Checking for isatty now

--- lib/console.hpp
+++ lib/console.hpp
@@ -2,9 +2,11 @@
 #define CONSOLE_HPP_
 
 #    ifndef _WIN32
-#        define ESC_GREEN "\033[32;1m"
-#        define ESC_RED "\033[31;1m"
-#        define ESC_CANCEL "\033[0;;m"
+#        include <unistd.h>
+#        define ESC_I(code) (isatty(fileno(stdout)) ? code : "")
+#        define ESC_GREEN ESC_I("\033[32;1m")
+#        define ESC_RED ESC_I("\033[31;1m")
+#        define ESC_CANCEL ESC_I("\033[0;;m")
 #    else
 #        define ESC_GREEN ""
 #        define ESC_RED ""
